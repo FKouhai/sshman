81197978ee1568fc7b70901029779e97208ee689
Fran Cirka <frandres00@gmail.com>
2021-02-19 20:58:07 +0100

    Add files via upload

--- sshConnect.py
+++ sshConnect.py
@@ -2,21 +2,24 @@ import getpass
 import pexpect
 from pexpect import pxssh
 servers = open('server.txt','r') 
-
 inf=open('info','a')
 
-for ip in servers:
-  print(ip)
-  user = input('username:' )
-  password = getpass.getpass('password: ')
-  s = pxssh.pxssh()
-  s.login(ip,user,password)
-  inf.write(ip)
-  s.sendline('df -h')
-  s.prompt()
-  inf.write(s.before)
-  s.sendline('free -h')
-  s.prompt()
-  inf.write(s.before)
-  s.logout()
+def action(cmnd):
+  with open("server.txt") as servers:
+    for ip in servers:
+      print ip
+      user = input('username:')
+      password=getpass.getpass('password:')
+      invoke(ip,user,password,cmnd,)
+      
 
+def invoke(ip,usr,passw,command):
+  s = pxssh.pxssh()
+  s.login(ip, usr, passw)
+  inf.write(ip)
+  s.sendline(command)
+  s.prompt()
+  inf.write(s.before)
+  s.logout
+
+action("cat /etc/resolv.conf")
