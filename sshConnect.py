f7eb2cb497e1e7f42135b41b04c23acdb4eccde6
FKouhai <frandres00@gmail.com>
2021-02-21 20:19:36 +0100

    Fixed sys arg

--- sshConnect.py
+++ sshConnect.py
@@ -24,15 +24,13 @@ def invoke(ip,usr,passw,command,inf):
 try:
  action(cmnd)
 except NameError:
-  if sys.argv[0] == '-h' or sys.argv[0] == '--help':
+  if sys.argv[1]== '-h' or sys.argv[1] == '--help':
      print(" This program needs 3 arguments to work, the remote user, its password and the command you want to execute,\n you also need a file called server.txt where the remote IP's are \n this program only works in windows so far")
   elif len(sys.argv) < 3:
-     print(sys.argv[0])
      print("You need to pass 3 args which are the user, the ssh pass and the command you want to run on the remote machine")
   elif len(sys.argv) == 3:
-     print(sys.argv[0])
-     user = sys.argv[0]
-     pasw = sys.argv[1]
-     cmnd = sys.argv[2]
+     user = sys.argv[1]
+     pasw = sys.argv[2]
+     cmnd = sys.argv[3]
 
 
