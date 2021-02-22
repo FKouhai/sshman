f587c34f3a440476210fb6a6d1e247c22b48a311
FKouhai <frandres00@gmail.com>
2021-02-22 21:10:34 +0100

    Fixed parsing

--- sshConnect.py
+++ sshConnect.py
@@ -30,9 +30,11 @@ try:
  action(cmnd)
 except NameError:
   if sys.argv[1]== '-h' or sys.argv[1] == '--help':
-     print(""" This program needs 3 arguments to work, the remote user,
+     print("""
+     This program needs 3 arguments to work, the remote user,
      its password and the command you want to execute,
      you also need a file called server.txt where the remote IP's are
-     this program only works in windows so far""")
+     this program only works in windows so far
+     """)
   elif len(sys.argv) < 3:
      print("You need to pass 3 args which are the user, the ssh pass and the command you want to run on the remote machine")
